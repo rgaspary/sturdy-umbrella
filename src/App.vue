7ca51cbd8a2d69f61f9297302c537d4e7471d688
Renzo Gaspary <rgaspary@gmail.com>
2020-10-06 11:13:08 -0400

    Fixing time and adding to main App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,36 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <Clock />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 import Clock from './components/Clock.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     Clock,
   }
 };
 </script>
 
 <style>
+body, html {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #333;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
   margin-top: 60px;
 }
 </style>
